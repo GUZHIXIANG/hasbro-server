e92bb85fc7dce11b81add57e5ed5d907f132c2a1
root <gzxsprite@hotmail.com>
2020-03-01 01:57:13 +0800

    修复省市区关联bug

--- gallant/wechatapp/models/AddressModel.py
+++ gallant/wechatapp/models/AddressModel.py
@@ -3,16 +3,16 @@ from django.contrib.auth.models import User
 
 
 # 行政区划表
-class Area(models.Model):
+class Areas(models.Model):
     id = models.CharField(verbose_name='行政编号', max_length=20, primary_key=True)
     name = models.CharField(verbose_name='行政区划名', max_length=20)
     parent_id = models.CharField(
         verbose_name='上级行政编号', max_length=20)
-    type = models.IntegerField(verbose_name='行政等级'）
+    type = models.IntegerField(verbose_name='行政等级',default=0)
 
     class Meta:
         db_table = 'wechatapp'
-        verbose_name = 'Area'
+        verbose_name = 'Areas'
         verbose_name_plural = '行政区划表'
 
     def __str__(self):
@@ -31,20 +31,20 @@ class Address(models.Model):
         verbose_name='手机号', max_length=20, blank=True)
     # 关联地区 ----- 省份
     province = models.ForeignKey(
-        Area, on_delete=models.SET_NULL)
+        Areas, on_delete=models.CASCADE,related_name='province')
     # 关联地区 ----- 城市
     city = models.ForeignKey(
-        Area, on_delete=models.SET_NULL)
+        Areas, on_delete=models.CASCADE,related_name='city')
     # 关联地区 ----- 区域
     district = models.ForeignKey(
-        Area, on_delete=models.SET_NULL)
+        Areas, on_delete=models.CASCADE,related_name='district')
     # 详细地址
     address = models.CharField(
         verbose_name='详细地址', max_length=256, blank=True)
     # 默认设置
-    CHOICE_TYPE = ((0,'否'),(1，'是'))
+    CHOICE_TYPE = ((0,'否'),(1,'是'))
     is_default = models.IntegerField(
-        verbose_name='默认设置'，choices=CHOICE_TYPE, default=0)
+        verbose_name='默认设置',choices=CHOICE_TYPE, default=0)
     # 创建时间
     signup_create_time = models.DateTimeField(
         verbose_name='创建时间', auto_now_add=True)
