83501d05eeaa0902f631c730ef351977e781d617
谢骏文 <digital_boundary@163.com>
2020-03-03 17:06:05 +0800

    123

--- gallant/wechatapp/models/ProductTypeModel.py
+++ gallant/wechatapp/models/ProductTypeModel.py
@@ -4,22 +4,69 @@ from stdimage.models import StdImageField
 
 # 商品分类表用于客户定义产品分类类别的
 # 这里使用一张表来完成分类，
-class ProductType(models.Model):
-    
+
+
+class ProductMainCategory(models.Model):
     # 商品总类 ------- 名称
     typeName = models.CharField('商品总类',max_length=50,blank=False)
 
+    def __str__(self):
+        return "["+self.typeName.__str__()+"]"
+
+    class Meta:
+        app_label = 'wechatapp'
+        verbose_name = '商品总类注册'
+        verbose_name_plural = '商品总类注册'
+
+
+class ProductSecondCategory(models.Model):
+    # 商品大类 ------- 名称
+    typeName = models.ForeignKey(ProductMainCategory,on_delete=models.CASCADE)
+
+    typeChildName =  models.CharField('商品大类',max_length=50,blank=False)
+
+    url = StdImageField(verbose_name="大类缩略图",upload_to='typea',variations={'nail': {'width': 10, 'height': 10}},default='')
+    
+    def __str__(self):
+        return "["+self.typeChildName.__str__()+"]"
+
+    class Meta:
+        app_label = 'wechatapp'
+        verbose_name = '商品大类注册'
+        verbose_name_plural = '商品大类注册'
+
+    def image_img(self):
+        if self.url:
+            return str('<img src="%s" />' % self.url.nail.url)
+        else:
+            return u'上传图片'
+
+    image_img.short_description = '图标'
+    image_img.allow_tags = True
+
+class ProductType(models.Model):
+    
     # 商品大类
-    typeChildName = models.CharField('商品大类',max_length=50,blank=False,default='大类产品')
+    typeChildName = models.ForeignKey(ProductSecondCategory,on_delete=models.CASCADE)
 
     # 商品子类
     typeChildsName = models.CharField('商品子类',max_length=50,blank=False,default='子类产品')
 
+    url = StdImageField(verbose_name="子类缩略图",upload_to='typeb',variations={'nail': {'width': 25, 'height': 25}},default='')
+
     def __str__(self):
-        return "["+self.typeName.__str__()+"]"+"["+self.typeChildName.__str__()+"]"+"["+self.typeChildsName.__str__()+"]"
+        return "["+self.typeChildsName.__str__()+"]"
 
     class Meta:
         app_label = 'wechatapp'
         verbose_name = '商品类别管理'
         verbose_name_plural = '商品类别管理'
-    
+
+    def image_img(self):
+        if self.url:
+            return str('<img src="%s" />' % self.url.nail.url)
+        else:
+            return u'上传图片'
+
+    image_img.short_description = '类别缩略图图'
+    image_img.allow_tags = True
