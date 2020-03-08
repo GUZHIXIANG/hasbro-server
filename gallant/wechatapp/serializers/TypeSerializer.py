d28f15fa1cfd870c958cf5f6d79f4e6e7355f851
谢骏文 <digital_boundary@163.com>
2020-03-08 17:49:43 +0800

    备份-03081749

--- gallant/wechatapp/serializers/TypeSerializer.py
+++ gallant/wechatapp/serializers/TypeSerializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from wechatapp.models.ProductTypeModel import ProductSecondCategory,ProductType,ProductMainCategory
+from wechatapp.models.ProductTypeModel import ProductSecondCategory,ProductType,ProductMainCategory,PType,PTypeImage
 
 
 class ProductSecondCategorySerializer(serializers.ModelSerializer):
@@ -16,3 +16,34 @@ class ProductMainCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductMainCategory
         fields = "__all__"
+
+
+class PTypeImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PTypeImage
+        fields = '__all__'
+
+
+class PTypeSerializer(serializers.ModelSerializer):
+
+    parent = serializers.SerializerMethodField()
+    desc = serializers.SerializerMethodField()
+    # image = serializers.SerializerMethodField()
+    images = serializers.SerializerMethodField()
+
+    class Meta:
+        model = PType
+        fields = ('id','parent','name','desc','images')
+    
+    def get_parent(self, obj):
+        return obj.parent.id
+    
+    def get_desc(self, obj):
+        return obj.get_desc_display()
+
+    # def get_image(self, obj):
+    #     return obj.type_image.get().image
+
+    def get_images(self, obj):
+        query_set = obj.type_image.all()
+        return [{'image': obj.image.thumbnail.__str__()} for obj in query_set]
